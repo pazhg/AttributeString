42c9501956b6dafce9127173c909939bdad42eb2
Kambiz Guity <kg@pazhg.com>
2019-03-07 15:53:38 +0330

    Second label with mutable attributes.

--- AttributeString/ViewController.swift
+++ AttributeString/ViewController.swift
@@ -12,18 +12,36 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelFirst: UILabel!
     
+    @IBOutlet weak var labelSecond: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-        // MARK: Label Attributes
-        
+        // MARK: First Label Attributes
         let dictionaryAttributeString : [ NSAttributedString.Key : Any ] = [NSAttributedString.Key.backgroundColor: UIColor.red]
         
-        let attributeString : NSAttributedString = NSAttributedString.init(string: "This is my label with standard attributes!", attributes: dictionaryAttributeString )
-        
+        let attributeString : NSAttributedString = NSAttributedString.init(string: "This is my first label with standard attributes!", attributes: dictionaryAttributeString )
         // Assign attributeString to labelFirst
         self.labelFirst.attributedText = attributeString
+        
+        
+        
+        
+        // MARK: Second Label Atributes ( MutableAttributes )
+        let str : String = "This is my second label with standard mutable attributes!"
+        let secondAttributeString : NSMutableAttributedString = NSMutableAttributedString.init(string: str)
+        
+        // NSRange gives us two parameters : Length & Location ;)
+        // NSMakeRange generate a NSRange
+        // This type of assigning parameters to NSRange is static
+        let range : NSRange = NSMakeRange( 0, "This is my second label".count )
+        
+        let attribute : [NSAttributedString.Key : Any] = [NSAttributedString.Key.foregroundColor : UIColor.red]
+        secondAttributeString.addAttributes(attribute, range: range)
+        
+        
+        //Assign attributes to labelSecond
+        self.labelSecond.attributedText = secondAttributeString as NSAttributedString
     
     }
 
