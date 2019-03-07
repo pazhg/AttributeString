640e32c7ab4094bf3f6df7c8d76e9934469eab7c
Kambiz Guity <kg@pazhg.com>
2019-03-07 14:48:35 +0330

    Initial Commit

---
+++ AttributeString/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttributeString
+//
+//  Created by Pazhg on 12/16/1397 AP.
+//  Copyright © 1397 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
