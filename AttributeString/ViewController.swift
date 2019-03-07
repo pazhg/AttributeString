a447261b155f14b5b7c218bcc6db25f88a82fe44
Kambiz Guity <kg@pazhg.com>
2019-03-07 15:25:46 +0330

    First Label with AttributeString

--- AttributeString/ViewController.swift
+++ AttributeString/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelFirst: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        // MARK: Label Attributes
+        
+        let dictionaryAttributeString : [ NSAttributedString.Key : Any ] = [NSAttributedString.Key.backgroundColor: UIColor.red]
+        
+        let attributeString : NSAttributedString = NSAttributedString.init(string: "This is my label with standard attributes!", attributes: dictionaryAttributeString )
+        
+        // Assign attributeString to labelFirst
+        self.labelFirst.attributedText = attributeString
+    
     }
 
 
